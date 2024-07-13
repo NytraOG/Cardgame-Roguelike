72a006fc8c6d39d5a981f6ecd5fbdfdf60ddef45
Christian Nytra <ch.nytra.13@gmail.com>
2024-07-13 15:04:04 +0200

    Init

---
+++ EventSystemSingleton.cs
@@ -0,0 +1,22 @@
+﻿// ReSharper disable InconsistentNaming
+
+namespace CardgameRoguelike;
+
+public sealed class EventSystemSingleton
+{
+    private static readonly EventSystemSingleton instance = null;
+    private static readonly object               padlock  = new();
+
+    private EventSystemSingleton() { }
+
+    public EventSystemSingleton Instance
+    {
+        get
+        {
+            lock (padlock)
+            {
+                return instance ?? new EventSystemSingleton();
+            }
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,10 @@
+using Godot;
+
+namespace CardgameRoguelike;
+
+public partial class Main : Control
+{
+    public override void _Ready() { }
+
+    public override void _Process(double delta) { }
+}
