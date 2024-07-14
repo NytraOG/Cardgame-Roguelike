9da9371885baca873c896cbaad45c1614e0bf80b
Christian Nytra <ch.nytra.13@gmail.com>
2024-07-14 12:04:04 +0200

    formatting hihihi

--- Cards/BaseCard.cs
+++ Cards/BaseCard.cs
@@ -4,11 +4,7 @@ namespace CardgameRoguelike.Cards;
 
 public partial class BaseCard : PanelContainer
 {
-	public override void _Ready()
-	{
-	}
+    public override void _Ready() { }
 
-	public override void _Process(double delta)
-	{
-	}
+    public override void _Process(double delta) { }
 }
