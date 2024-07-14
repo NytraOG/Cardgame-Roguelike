a3106bfbb8746ecd4c79dbf47fb41cfd3b7edce6
Christian Nytra <ch.nytra.13@gmail.com>
2024-07-14 12:01:51 +0200

    Basecard struktur OK

---
+++ Cards/BaseCard.cs
@@ -0,0 +1,14 @@
+using Godot;
+
+namespace CardgameRoguelike.Cards;
+
+public partial class BaseCard : PanelContainer
+{
+	public override void _Ready()
+	{
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
